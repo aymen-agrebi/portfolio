6eca51d8d2eb417aa2e220d2d1a83e46763eec48
aymen <aymen.agrebi.dev@gmail.com>
2022-01-10 15:23:33 +0100

    form deploy test

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,29 +1,22 @@
-import React, {useState} from "react";
+import React from "react";
 import classes from './Contact.module.css';
 import NeonButton from '../UI/NeonButton/NeonButton'
 
 const Contact = () => {
-  
-  const [form, setform] = useState({
-    name: null,
-    email: null,
-    subject: null,
-    message: null
-  });
 
   return (
     <div className={classes.Contact} id="contact">
       <div>
         <h2>Contact Me</h2>
-        <form name="contact" netlify>
-          <input type="text" name="name" placeholder="Name" 
-          onChange={(event )=> setform({...form, name: event.target.value})}/>
-          <input type="email" name="email"  placeholder="Email" 
-          onChange={(event )=> setform({...form, email: event.target.value})}/>
-          <textarea type="textArea" name="message" placeholder="Message" cols={30} rows={10}
-          onChange={(event )=> setform({...form, message: event.target.value})} > 
+        <form 
+        name="contact" 
+        method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email"  placeholder="Email" />
+          <textarea type="textArea" name="message" placeholder="Message" cols={30} rows={10} > 
           </textarea>
-          <NeonButton >Send <i class="fa fa-paper-plane" aria-hidden="true"></i></NeonButton>
+          <NeonButton buttonType="submit">Send <i class="fa fa-paper-plane" aria-hidden="true"></i></NeonButton>
         </form>
       </div>
     </div>
--- src/components/UI/NeonButton/NeonButton.js
+++ src/components/UI/NeonButton/NeonButton.js
@@ -3,8 +3,7 @@ import classes from './NeonButton.module.css';
 
 const NeonButton = (props) => {
     return (
-        <button onClick={props.clicked} className={classes.NeonButton} >{props.children}</button>
+        <button onClick={props.clicked} className={classes.NeonButton} type={props.buttonType}>{props.children}</button>
     )
 }
-
 export default NeonButton
